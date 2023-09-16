764514c41d99e59a68759686fae05a84f5724e4d
OliveMonk <gp7.official@gmail.com>
2023-09-16 23:21:55 +0530

    added image upload route

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express');
-require('dotenv').config();
-
+require('dotenv').config()
+const uploadRoute = require('./controller/routeUpload');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 
-
-// route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//the route 
+app.use("/api/users" , uploadRoute);
 
 
-// listen
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
-})
+//port connection 
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
